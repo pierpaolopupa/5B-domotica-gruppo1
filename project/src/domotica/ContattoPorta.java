3a1bf173c2a81d87d517588de05aa684c07493ba
Luca <alessio.terzi@leomail.it>
2025-11-13 08:48:51 +0100

    Sala: client ContattoPorta

--- project/src/domotica/ContattoPorta.java
+++ project/src/domotica/ContattoPorta.java
@@ -1,5 +1,58 @@
 package domotica;
 
+import java.io.*;
+import java.net.*;
 public class ContattoPorta {
+  String nomeServer ="localhost";                  // indirizzo server locale  
+  int portaServer   = 6789;                        // porta x servizio data e ora
+  Socket miosocket;                                
+  BufferedReader tastiera;                         // buffer per l'input da tastiera
+  String stringaUtente;                            // stringa inserita da utente
+  String stringaRicevutaDalServer;                 // stringa ricevuta dal server
+  DataOutputStream outVersoServer;                 // stream di output
+  BufferedReader inDalServer;                      // stream di input 
 
+  public void comunica() {
+    for (;;)                                     // ciclo infinito: termina con FINE
+    try{
+      
+      }
+    } 
+    catch (Exception e) 
+    {
+      System.out.println(e.getMessage());
+      System.out.println("Errore durante la comunicazione col server!");
+      System.exit(1);
+    }
+  }
+  
+  public Socket connetti(){
+    System.out.println("2 CLIENT partito in esecuzione ...");
+    try{
+      // input da tastiera
+      tastiera = new BufferedReader(new InputStreamReader(System.in));
+      //  miosocket = new Socket(InetAddress.getLocalHost(), 6789);
+      miosocket = new Socket(nomeServer,portaServer);
+      // associo due oggetti al socket per effettuare la scrittura e la lettura 
+      outVersoServer = new DataOutputStream(miosocket.getOutputStream());
+      inDalServer    = new BufferedReader(new InputStreamReader (miosocket.getInputStream()));
+    } 
+    catch (UnknownHostException e){
+      System.err.println("Host sconosciuto"); } 
+    catch (Exception e){
+      System.out.println(e.getMessage());
+      System.out.println("Errore durante la connessione!");
+      System.exit(1);
+    }
+    return miosocket;
+  }
+
+  public static void main(String args[]) {
+    ContattoPorta cliente = new ContattoPorta();
+    cliente.connetti();
+    cliente.comunica();
+  }   
 }
+
+
+
