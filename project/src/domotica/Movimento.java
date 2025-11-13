720f13c7a01a80a3d53a4579b3e9737895bb9914
Luca <5B_INF_PUPA@CENTROSTUDI.local>
2025-11-13 08:18:24 +0100

    Monetti: inizializzazione Client Movimento

--- project/src/domotica/Movimento.java
+++ project/src/domotica/Movimento.java
@@ -1,5 +1,51 @@
 package domotica;
 
+import java.io.*;
+import java.net.*;
 public class Movimento {
+  String nomeServer ="localhost";              
+  int portaServer   = 6789;               
+  Socket miosocket;                
+  BufferedReader tastiera;                 
+  String stringaUtente;
+  String stringaRicevutaDalServer;          
+  DataOutputStream outVersoServer;              
+  BufferedReader inDalServer;                  
 
+  public void comunica() {
+    for (;;)                                    
+    try{
+      
+    } 
+    catch (Exception e) 
+    {
+      System.out.println(e.getMessage());
+      System.out.println("Errore durante la comunicazione col server!");
+      System.exit(1);
+    }
+  }
+  
+  public Socket connetti(){
+    System.out.println("2 CLIENT partito in esecuzione ...");
+    try{
+      tastiera = new BufferedReader(new InputStreamReader(System.in));
+      miosocket = new Socket(nomeServer,portaServer); 
+      outVersoServer = new DataOutputStream(miosocket.getOutputStream());
+      inDalServer    = new BufferedReader(new InputStreamReader (miosocket.getInputStream()));
+    } 
+    catch (UnknownHostException e){
+      System.err.println("Host sconosciuto"); } 
+    catch (Exception e){
+      System.out.println(e.getMessage());
+      System.out.println("Errore durante la connessione!");
+      System.exit(1);
+    }
+    return miosocket;
+  }
+
+  public static void main(String args[]) {
+    Movimento cliente = new Movimento();
+    cliente.connetti();
+    cliente.comunica();
+  }   
 }
