f95cf3c7d1d3dbafdbff7a08442400bf85343307
Luca <alessio.terzi@leomail.it>
2025-11-19 11:02:06 +0100

    Monetti: inizializzazione variabili e invio dei dati al server

--- project/src/domotica/Movimento.java
+++ project/src/domotica/Movimento.java
@@ -2,20 +2,63 @@ package domotica;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.time.*;
+
 public class Movimento {
   String nomeServer ="localhost";              
   int portaServer   = 6789;               
   Socket miosocket;                
-  BufferedReader tastiera;                 
-  String stringaUtente;
-  String stringaRicevutaDalServer;          
+  Scanner tastiera;                 
+  String id;
+  String tipo="movimento";
+  boolean valore;              
+  String zona;
+  LocalTime ora;
   DataOutputStream outVersoServer;              
   BufferedReader inDalServer;                  
 
   public void comunica() {
     for (;;)                                    
     try{
-      
+    	outVersoServer.writeBytes("Tipo: "+tipo);
+    	
+    	String risp;
+    	do {
+    		System.out.println("È stato rivelato un movimento?");
+    		risp=tastiera.nextLine();
+    	}while(!risp.equalsIgnoreCase("si") || !risp.equalsIgnoreCase("no"));
+    	
+    	if(risp.equalsIgnoreCase("si")) {
+    		valore=true;
+    		}
+    	else {
+    		valore=false;
+    	}
+    	
+    	outVersoServer.writeBytes("Valore:"+valore);
+    	
+    	
+    	String zona;
+    	do {
+    		System.out.println("In che zona è stato rilevato il movimento?");
+    		zona=tastiera.nextLine();
+    	}while(!zona.equalsIgnoreCase("giardino") || !zona.equalsIgnoreCase("cucina") || !zona.equalsIgnoreCase("bagno"));
+    	outVersoServer.writeBytes("Zona: "+zona);
+    	
+    	
+    	int ore, minuti;
+    	do {
+    		System.out.println("Inserisci le ore");
+   			ore=tastiera.nextInt();
+   		}while(ore<0 || ore>23);
+   		do {
+   			System.out.println("Inserisci i minuti");
+    		minuti=tastiera.nextInt();
+    	}while(minuti<0 || minuti>59);
+   		ora=LocalTime.of(ore, minuti);
+   		outVersoServer.writeBytes("Ora: "+ora);	
+    	
     } 
     catch (Exception e) 
     {
@@ -26,9 +69,9 @@ public class Movimento {
   }
   
   public Socket connetti(){
-    System.out.println("2 CLIENT partito in esecuzione ...");
+    System.out.println("CLIENT partito in esecuzione ...");
     try{
-      tastiera = new BufferedReader(new InputStreamReader(System.in));
+      tastiera = new Scanner(System.in);
       miosocket = new Socket(nomeServer,portaServer); 
       outVersoServer = new DataOutputStream(miosocket.getOutputStream());
       inDalServer    = new BufferedReader(new InputStreamReader (miosocket.getInputStream()));
