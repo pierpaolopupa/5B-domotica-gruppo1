cd0d8729e21433b00bfcb85c95ec8f3b8f912cbb
Scuola-smth <alessandro.sala1@leomail.it>
2025-11-20 23:48:26 +0100

    Sala:Fine contatto porta

--- project/src/domotica/ContattoPorta.java
+++ project/src/domotica/ContattoPorta.java
@@ -1,34 +1,56 @@
 package domotica;
+import java.util.*;
+
 
 import java.io.*;
 import java.net.*;
 public class ContattoPorta {
-  String nomeServer ="localhost";                  // indirizzo server locale  
-  int portaServer   = 6789;                        // porta x servizio data e ora
+  String nomeServer ="localhost";                    
+  int portaServer   = 6789;                        
   Socket miosocket;                                
-  BufferedReader tastiera;                         // buffer per l'input da tastiera
-  DataOutputStream outVersoServer;                 // stream di output
-  BufferedReader inDalServer;                      // stream di input 
-  String id;
-  boolean valoreC;
-  boolean conttato;
+  Scanner tastiera;
+  DataOutputStream outVersoServer;                 
+  BufferedReader inDalServer;                      
+  String id="1";
+  String tipo="contatto";
+  boolean valore;
   String zona;
-
-  public ContattoPorta(String id, String valoreC, boolean contatto, String zona) {
-        this.id= id;
-        this.tipo = tipo;
-        this.contatto = valore;
-        this.zona = zona;
-    }
-
   
 
   public void comunica() {
-    for (;;)                                     // ciclo infinito: termina con FINE
+    for (;;)                                     
     try{
-      
+    	String risp;
+    	
+    	
+    	
+    	System.out.println("Il contatto si è interrotto?");
+    	risp=tastiera.nextLine();
+    	if(risp.equalsIgnoreCase("si"))
+    	{
+    		valore=true;
+    		
+    		do {
+    			System.out.println("In quale zona?");
+        		zona=tastiera.nextLine();
+        	}while(!zona.equalsIgnoreCase("ingresso") || !zona.equalsIgnoreCase("cucina") || !zona.equalsIgnoreCase("garage"));
+    		outVersoServer.writeBytes("Id: "+id+",\nTipo: "+tipo+",\nValore: "+valore+",\nZona: "+zona);
+    		
+    	}else{
+    		valore=false;
+    	}
+    	outVersoServer.writeBytes("Id: "+id+",\nTipo: "+tipo+",\nValore: "+valore);
+    	System.out.println("Risposta del server: "+inDalServer.readLine());
+    	
+    	System.out.println("Inserisci 'FINE' se vuoi chiudere la connessione");
+		String finito=tastiera.nextLine();
+		if(finito.equalsIgnoreCase("FINE")) {
+			miosocket.close();
+		}
       }
-    } 
+    
+   
+     
     catch (Exception e) 
     {
       System.out.println(e.getMessage());
@@ -40,11 +62,11 @@ public class ContattoPorta {
   public Socket connetti(){
     System.out.println("2 CLIENT partito in esecuzione ...");
     try{
-      // input da tastiera
-      tastiera = new BufferedReader(new InputStreamReader(System.in));
-      //  miosocket = new Socket(InetAddress.getLocalHost(), 6789);
+      
+      tastiera = new Scanner(System.in);
+      
       miosocket = new Socket(nomeServer,portaServer);
-      // associo due oggetti al socket per effettuare la scrittura e la lettura 
+       
       outVersoServer = new DataOutputStream(miosocket.getOutputStream());
       inDalServer    = new BufferedReader(new InputStreamReader (miosocket.getInputStream()));
     } 
